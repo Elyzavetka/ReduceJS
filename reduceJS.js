c95f9a9b6534e9e45cdb945a0df4c57a0c307491
Elyzavetka <lizamybella@gmail.com>
2023-02-25 16:15:50 +0000

    Update reduceJS.js

--- reduceJS.js
+++ reduceJS.js
@@ -1,7 +1,18 @@
-let a = [22, 33, 44];
+//let a = [22, 33, 44];
+
+//let b = a.reduce((accum, item) => {
+//  accum = accum + item;
+//  return accum;
+//});
+//console.log("=========");
+//console.log(b);
+
+let a = [4, -5, 7, -6, 2];
 
 let b = a.reduce((accum, item) => {
-  accum = accum + item;
+  if (item > 0) {
+    accum += item;
+  }
   return accum;
 });
 console.log("=========");
